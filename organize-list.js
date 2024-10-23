f6fedd7b2713d3fa0b1c6326465b346779adafa3
Tiaotiao <65841827@qq.com>
2024-10-23 21:17:41 +0800

    Update organize-list.js

--- organize-list.js
+++ organize-list.js
@@ -9,7 +9,7 @@ function orzl(){
 function orz(n){ return (orzl())[n]; };
 
 var htor='';
-for(a=0, a<orzl().length, a++){
+for(a=0; a<orzl().length; a++){
  htor+='<div class="orz"><h3>'+orz(a).name+'</h3><pre>创建者:'+orz(a).createAuthor+'\n创建时间:'+orz(a).createTime+'</pre></div>';
 }
 
