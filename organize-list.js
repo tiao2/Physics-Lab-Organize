00ea963fcbae36765c88a61a3b3595c641760ecd
Tiaotiao <65841827@qq.com>
2024-10-23 20:31:56 +0800

    Update organize-list.js

--- organize-list.js
+++ organize-list.js
@@ -1,10 +1,15 @@
 function orzl(){
-  return [];
+  return [{
+    "name": "海龟实验室社区委员会",
+    "createAuthor": "未知",
+    "createTime": "未知"
+  }];
 }
 
 function orz(n){ return (orzl())[n]; };
 
 var htor='';
 for(a=0, a<orzl().length, a++){
- htor='<div class="orz"><h3>'+orz(a).name+'</h3></div>';
+ htor+='<div class="orz"><h3>'+orz(a).name+'</h3><pre>创建者:'+orz(a).createAuthor+'\n创建时间:'+orz(a).createTime+'</pre></div>';
 }
+//document.getElement
