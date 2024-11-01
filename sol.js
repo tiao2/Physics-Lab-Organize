db90f2121e7bcb22c0bfcb6aadd7332b3c752bf8
Tiaotiao <65841827@qq.com>
2024-11-01 20:01:44 +0800

    Update sol.js

--- sol.js
+++ sol.js
@@ -1,1 +1,15 @@
+function gqu() {
+    let queryParams = {};
+    let ur = window.location.href;
+    let queryString = ur.split('?')[1];
+    if (queryString) {
+        let params = queryString.split('&');
+        params.forEach(function(param) {
+            let split = param.split('=');
+            queryParams[split[0]] = split[1];
+        });
+    }
+    return queryParams.orz;
+};
 
+document.getElementById('search').value=(gqu()==undefined)?'':gqu();
