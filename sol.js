2355817d7210162f83b407c62f7c89cef7e644b6
Tiaotiao <65841827@qq.com>
2024-11-03 02:25:48 +0000

    Update sol.js

--- sol.js
+++ sol.js
@@ -23,48 +23,48 @@ function decodeBase64(base64Str) {
 function orzl(){
   return [{
    name: "星尘报社总部",
-   createAuthor: "",
-   createTime: "",
+   createAuthor: "iceiceiceice",
+   createTime: "2022.04.15",
   },{
     name: "服务用户协会",
-    createAuthor: "",
-    createTime: ""
+    createAuthor: "沃尔夫冈",
+    createTime: "2023.07.11"
   },{
     name: "物实之家-解忧咨询室",
-    createAuthor: "",
-    createTime: ""
+    createAuthor: "生巧",
+    createTime: "2022.08.09"
   },{
  name: "物实法理学协会",
- createAuthor: "",
- createTime: ""
+ createAuthor: "逐影星空",
+ createTime: "2023.02.25"
   },{
     name: "互助团",
-    createAuthor: "",
-    createTime: ""
+    createAuthor: "钇Yttrium",
+    createTime: "2023.01.24"
   },{
     name: "逐影联盟",
-    createAuthor: "",
-    createTime: ""
+    createAuthor: "逐影星空",
+    createTime: "2021.10.05"
   },{
     name: "星辉报社",
-    createAuthor: "",
-    createTime: ""
+    createAuthor: "沃尔夫冈",
+    createTime: "2023.02.03"
   },{
     name: "加强物理实验室黑洞区基础学科建设计划(强基计划)",
-    createAuthor: "",
-    createTime: ""
+    createAuthor: "班长大大",
+    createTime: "2022.04.21"
   },{
     name: "理颜计划",
-    createAuthor: "",
-    createTime: ""
+    createAuthor: "无产阶级编辑",
+    createTime: "2022.08.07"
   },{
     name: "晚会聊天室",
-    createAuthor: "",
-    createTime: ""
+    createAuthor: "是橙子呀~",
+    createTime: "2022.08.30"
   },{
     name: "微风周刊刊社",
-    createAuthor: "",
-    createTime: ""
+    createAuthor: "眺跳",
+    createTime: "2023.02.14"
   }];
 }
 
